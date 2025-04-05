0a006be6f5c727c5da02a7feca82bd7c9482ef13
Austin Ford <j.austin.ford@gmail.com>
2025-04-04 20:33:59 -0600

    reorg local dev

--- triggers/profile.py
+++ triggers/profile.py
@@ -5,9 +5,13 @@ and pause the screen.
 
 import wrappers
 
+PROFILE = input("\n \033[31mprofile\033[0m : ")
+
+print(" ")
+
 wrappers.compose_up("builder", "build")
-wrappers.compose_up("secrets-encrypt", "up --detach")
-wrappers.compose_logs("secrets-encrypt")
-wrappers.compose_down("secrets-encrypt")
+wrappers.compose_up(PROFILE, "up --detach")
+wrappers.compose_logs(PROFILE)
+wrappers.compose_down(PROFILE)
 
 input("\nPress [enter] to continue... ")
