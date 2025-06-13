4f47a661bb21f50d6a139c86d1212c8d986fcf24
james austin <j.austin.ford@gmail.com>
2025-06-13 13:00:24 -0600

    break out all templates - #6

---
+++ ansible/roles/elise.tunnels/templates/tunnel.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+autossh -N -T \
+    -o ServerAliveInterval=30 \
+    -o ServerAliveCountMax=960 \
+    -o ExitOnForwardFailure=yes \
+    -o StrictHostKeyChecking=no \
+    -o UserKnownHostsFile=/dev/null \
+    -i /root/ssh.key \
+    -R {{ item.external_port }}:{{ item.ipv4 }}:{{ item.internal_port }} \
+    root@portal.elysianskies.com
